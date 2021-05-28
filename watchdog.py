340a03b9f2b1dcf77ea94a3b205c936762312521
ThomasRgbg <ThomasRgbg@users.noreply.github.com>
2021-05-28 12:03:39 +0200

    Watchdog function via timer

---
+++ watchdog.py
@@ -0,0 +1,21 @@
+from machine import Timer, reset 
+
+
+class TimerWatchdog:
+    def __init__(self, interval):
+        self.timer = Timer(0)
+        self.timer.init(period=(interval*1000), mode=Timer.PERIODIC, callback=self.wdtcheck)
+        self.feeded = True
+
+    def wdtcheck(self, timer):
+        if self.feeded:
+            print("Watchdog feeded, all fine")
+            self.feeded = False
+        else:
+            print("Watchdog hungry, lets do a reset in 5 sec")
+            time.sleep(5)
+            reset()
+
+    def feed(self):
+        self.feeded = True
+
